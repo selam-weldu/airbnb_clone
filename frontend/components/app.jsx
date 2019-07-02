ad484e8650c7e5abb6ad8db93c5d3cf0efd5a46c
Selam Weldu <ssweldu@gmail.com>
2019-07-02 15:31:00 -0700

    Implement AuthRoute

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import GreetingContainer from './greeting/greeting_container';
-import {Route} from 'react-router-dom';
+import {
+    Route,
+    Switch
+} from 'react-router-dom';
 import LoginFormContainer from '../components/session_form/login_form_container';
 import SignupFormContainer from '../components/session_form/signup_form_container';
 
@@ -12,9 +14,10 @@ const App = () => (
             <h1>AirBnB Clone</h1>
             <GreetingContainer />
         </header>
-
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+        <Switch>
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch>
     </div>
 );
 export default App;
