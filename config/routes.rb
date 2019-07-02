79108c2fa75d1fc347afbaba5b5d2c77e3360950
Selam Weldu <ssweldu@gmail.com>
2019-07-02 14:01:56 -0700

    Finish sessionApiUtils

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:index, :create, :show]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
+  root "static_pages#root"
 end
---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,22 @@
+export const signup = user => (
+    $.ajax({
+        method: 'Post',
+        url: '/api/users',
+        data: { user }
+    })
+);
+
+export const login = user => (
+    $.ajax({
+        method: 'Post',
+        url: '/api/session',
+        data: { user }
+    })
+);
+
+export const logout = () => (
+    $.ajax({
+        method: 'Delete',
+        url: '/api/session',
+    })
+);
