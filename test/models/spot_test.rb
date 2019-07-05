330c9b8352d7bb0ef035631baf3f352f4b0f029d
Selam Weldu <ssweldu@gmail.com>
2019-07-04 21:28:51 -0700

    Migrated spots table

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -5,23 +5,23 @@
 #  id           :bigint           not null, primary key
 #  title        :string           not null
 #  description  :text             not null
-#  type         :string
-#  host_id      :integer          not null
-#  price        :float            not null
-#  num_rooms    :integer
+#  num_guests   :integer
 #  num_bedrooms :integer
 #  num_beds     :integer
-#  num_guests   :integer
+#  num_baths    :integer
+#  listing_type :string
+#  price        :float            not null
+#  location     :string           not null
+#  loc_detail   :string
+#  latitude     :float            not null
+#  longitude    :float            not null
+#  host_id      :integer          not null
 #  wifi         :boolean          default(TRUE)
 #  washer       :boolean          default(TRUE)
-#  dryer        :boolean          default(TRUE)
+#  tv           :boolean          default(TRUE)
 #  elevator     :boolean          default(TRUE)
 #  parking      :boolean          default(TRUE)
 #  kitchen      :boolean          default(TRUE)
-#  location     :string           not null
-#  loc_detail   :string
-#  longitude    :float            not null
-#  latitude     :float            not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
