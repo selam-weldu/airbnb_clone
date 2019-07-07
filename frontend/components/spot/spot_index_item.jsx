19adce2b77df5b3fe7761c87a30ce163f62d100b
Selam Weldu <ssweldu@gmail.com>
2019-07-06 20:04:19 -0700

    Implement carousel

--- frontend/components/spot/spot_index_item.jsx
+++ frontend/components/spot/spot_index_item.jsx
@@ -1,42 +1,28 @@
 import React from 'react';
+import Carousel from 'nuka-carousel';
+
 
 const SpotIndexItem = ({ spot }) => {
 
-//     let photoUrl;
-//     spot.photoUrls.forEach(url => (
-//         photoUrl = url;
-//     ));
 
-// let photoUrl;
-// if (listing.photoUrls.length >= 1) {
-//     photoUrl = listing.photoUrls[0];
-// }
-   
+
     return (
         <div className="spot-index-item">
             <li className="spot-li">
                
-                <div className="slideshow-container">
-                    <div className="spot-photos mySlides fade">
-                        <img src={spot.photoUrls[0]} className="spot-photos"/>
-                        <img src={spot.photoUrls[1]} className="spot-photos"/>
-                        <img src={spot.photoUrls[1]} className="spot-photos"/>
-                        <img src={spot.photoUrls[3]} className="spot-photos"/>
-                        <img src={spot.photoUrls[4]} className="spot-photos"/>
-                        <img src={spot.photoUrls[5]} className="spot-photos"/>
-                        <img src={spot.photoUrls[6]} className="spot-photos"/>
-                        <img src={spot.photoUrls[7]} className="spot-photos"/>
+                     <div className="spot-photos">
+                         <Carousel>
+                            <img src={spot.photoUrls[0]} className="spot-photos"/>
+                            <img src={spot.photoUrls[1]} className="spot-photos"/>
+                            <img src={spot.photoUrls[2]} className="spot-photos"/>
+                            <img src={spot.photoUrls[3]} className="spot-photos"/>
+                            <img src={spot.photoUrls[4]} className="spot-photos"/>
+                            <img src={spot.photoUrls[5]} className="spot-photos"/>
+                            <img src={spot.photoUrls[6]} className="spot-photos"/>
+                            <img src={spot.photoUrls[7]} className="spot-photos"/>
+                        </Carousel>
+                     </div>
 
-                        <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
-                        <a className="next" onclick="plusSlides(1)">&#10095;</a>
-
-                        {/* <div style="text-align:center">
-                            <span className="dot" onClick="currentSlide(1)"></span>
-                            <span className="dot" onClick="currentSlide(2)"></span>
-                            <span className="dot" onClick="currentSlide(3)"></span>
-                        </div> */}
-                    </div>
-                </div>
 
                 <div className="spot-info">
                     <div className="spot-short-info">
