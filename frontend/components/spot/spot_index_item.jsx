23894425fc5a46699826dd6d768fa039cfea05b5
Selam Weldu <ssweldu@gmail.com>
2019-07-06 19:20:12 -0700

    Images show up on index page

--- frontend/components/spot/spot_index_item.jsx
+++ frontend/components/spot/spot_index_item.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 
 const SpotIndexItem = ({ spot }) => {
 
+//     let photoUrl;
+//     spot.photoUrls.forEach(url => (
+//         photoUrl = url;
+//     ));
+
+// let photoUrl;
+// if (listing.photoUrls.length >= 1) {
+//     photoUrl = listing.photoUrls[0];
+// }
    
     return (
         <div className="spot-index-item">
             <li className="spot-li">
                
-                <div className="spot-photos">
-                    <img src={spot.photoUrl} className="spot-photos"/>
+                <div className="slideshow-container">
+                    <div className="spot-photos mySlides fade">
+                        <img src={spot.photoUrls[0]} className="spot-photos"/>
+                        <img src={spot.photoUrls[1]} className="spot-photos"/>
+                        <img src={spot.photoUrls[1]} className="spot-photos"/>
+                        <img src={spot.photoUrls[3]} className="spot-photos"/>
+                        <img src={spot.photoUrls[4]} className="spot-photos"/>
+                        <img src={spot.photoUrls[5]} className="spot-photos"/>
+                        <img src={spot.photoUrls[6]} className="spot-photos"/>
+                        <img src={spot.photoUrls[7]} className="spot-photos"/>
+
+                        <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+                        <a className="next" onclick="plusSlides(1)">&#10095;</a>
+
+                        {/* <div style="text-align:center">
+                            <span className="dot" onClick="currentSlide(1)"></span>
+                            <span className="dot" onClick="currentSlide(2)"></span>
+                            <span className="dot" onClick="currentSlide(3)"></span>
+                        </div> */}
+                    </div>
                 </div>
 
                 <div className="spot-info">
@@ -30,8 +57,8 @@ const SpotIndexItem = ({ spot }) => {
                         </div>
                     </div>
                     <div className="spot-footer-info">
-                        <div>4.9({spot.id - 100 })</div>  
-                        <div>${spot.price}/night</div>
+                        <p>&#9733;4.9({spot.id - 100 })</p>  
+                        <p>${spot.price}/night</p>
                     </div>
                 </div>
             </li>
