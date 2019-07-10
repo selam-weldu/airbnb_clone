114088794fe75e6ebdf2c3615f97ca7f5c16a7d3
Selam Weldu <ssweldu@gmail.com>
2019-07-10 15:13:01 -0700

    resize components on show

--- frontend/components/spot/spot_show/location.jsx
+++ frontend/components/spot/spot_show/location.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import SpotMapContainer from '../spot_map_container'
 
-const Location = () => {
+const Location = ({loc_detail}) => {
     return (
         <div className="location-container">
+            <div className="thin-line-location"></div>
          <div className="location">
             <p>Location</p>     
         </div>  
 
         <div className="address">
-            <p>San Francisco, California, United States</p>
+            <span>San Francisco, California, United States</span>
+        </div>
+
+        <div className="loca_detail">
+            <span>{loc_detail}</span>
         </div>
 
         <div className="show-map">
